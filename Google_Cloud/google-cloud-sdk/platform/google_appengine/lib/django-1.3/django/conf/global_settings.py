345391b9723601655838996367f8c43558976a72
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-25 19:08:36 +0300

    Removal of master/slave jargon

--- Google_Cloud/google-cloud-sdk/platform/google_appengine/lib/django-1.3/django/conf/global_settings.py
+++ Google_Cloud/google-cloud-sdk/platform/google_appengine/lib/django-1.3/django/conf/global_settings.py
@@ -209,7 +209,7 @@ TEMPLATE_STRING_IF_INVALID = ''
 
 # Default e-mail address to use for various automated correspondence from
 # the site managers.
-DEFAULT_FROM_EMAIL = 'webmaster@localhost'
+DEFAULT_FROM_EMAIL = 'webmain@localhost'
 
 # Subject-line prefix for email messages send with django.core.mail.mail_admins
 # or ...mail_managers.  Make sure to include the trailing space.
